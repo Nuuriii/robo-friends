74abeff577268e2c377aaf13e00d5e6f60b37d5d
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-09-26 21:14:42 +0700

    create initial files for Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,11 @@
+export const Card = () => {
+   return (
+      <div>
+         <img src='' alt='robots' />
+         <div>
+            <h2>Jane Doe</h2>
+            <p>jane.doe@gmail.com</p>
+         </div>
+      </div>
+   );
+};
