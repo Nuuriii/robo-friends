d3ed6fbcb248a8737d832ac3ccd531e9e216fb9c
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-09-26 23:15:17 +0700

    looping card directly in the rendering component

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -2,9 +2,11 @@ import { Card } from '../Card/Card';
 import { Robots } from './robots';
 
 export const CardList = () => {
-  const LoopCard = Robots.map((user, i) => {
-    return <Card key={i} id={user.id} name={user.name} email={user.email} />;
-  });
-
-  return <>{LoopCard}</>;
+  return (
+    <>
+      {Robots.map((user, i) => (
+        <Card key={i} id={user.id} name={user.name} email={user.email} />
+      ))}
+    </>
+  );
 };
