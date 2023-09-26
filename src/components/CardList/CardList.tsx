64534af183781c7e644e84473da932fb7a8ff923
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-09-26 22:35:43 +0700

    displays card components using looping

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -2,12 +2,9 @@ import { Card } from '../Card/Card';
 import { Robots } from './robots';
 
 export const CardList = () => {
-  return (
-    <>
-      <Card id={Robots[0].id} name={Robots[0].name} email={Robots[0].email} />
-      <Card id={Robots[1].id} name={Robots[1].name} email={Robots[1].email} />
-      <Card id={Robots[2].id} name={Robots[2].name} email={Robots[2].email} />
-      <Card id={Robots[3].id} name={Robots[3].name} email={Robots[3].email} />
-    </>
-  );
+  const LoopCard = Robots.map((user) => {
+    return <Card id={user.id} name={user.name} email={user.email} />;
+  });
+
+  return <>{LoopCard}</>;
 };
--- src/components/CardList/robots.ts
+++ src/components/CardList/robots.ts
@@ -1,26 +1,38 @@
 export const Robots = [
-   {
-      id: 0,
-      name: "Richard",
-      username: "Rich",
-      email: "richard@gmail.com",
-   },
-   {
-      id: 1,
-      name: "John Doe",
-      username: "John",
-      email: "johndoe@gmail.com",
-   },
-   {
-      id: 2,
-      name: "Meggy Doe",
-      username: "Meggy",
-      email: "meggy@gmail.com",
-   },
-   {
-      id: 3,
-      name: "Nemo Bot",
-      username: "Nemo",
-      email: "nemobot@gmail.com",
-   },
+  {
+    id: 0,
+    name: 'Richard',
+    username: 'Rich',
+    email: 'richard@gmail.com',
+  },
+  {
+    id: 1,
+    name: 'John Doe',
+    username: 'John',
+    email: 'johndoe@gmail.com',
+  },
+  {
+    id: 2,
+    name: 'Meggy Doe',
+    username: 'Meggy',
+    email: 'meggy@gmail.com',
+  },
+  {
+    id: 3,
+    name: 'Nemo Bot',
+    username: 'Nemo',
+    email: 'nemobot@gmail.com',
+  },
+  {
+    id: 4,
+    name: 'Roronoa Zoro',
+    username: 'Zoro',
+    email: 'zoro@gmail.com',
+  },
+  {
+    id: 5,
+    name: 'Luffy Bot',
+    username: 'Luffy',
+    email: 'luffy@gmail.com',
+  },
 ];
