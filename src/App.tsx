17f7680401d3f1b47c9ffde3092c7144ed6589fb
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-09-26 23:40:21 +0700

    set the rendering results of the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { SearchBox } from './components/searchBox/SearchBox';
 import { CardList } from './components/CardList/CardList';
 
 export const App = () => {
   return (
-    <>
+    <div>
+      <h1>Robo Friends</h1>
+      <SearchBox />
       <CardList />
-    </>
+    </div>
   );
 };
